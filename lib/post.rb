aa5972aee4de99c4da53085fe5c64f53a1b27340
mjester93 <m.jester.93@gmail.com>
2020-06-04 19:15:45 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,25 @@
+class Post
+
+    attr_accessor :title, :author
+
+    @@all = []
+
+    def initialize(title)
+        @title = title
+
+        @@all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        if !self.author.nil?
+            return self.author.name
+        else
+            return nil
+        end
+    end
+
+end
