ce49a6eec927d58c0ae775f3efda24541487001b
BartTheScrivner <dframe22@gmail.com>
2020-06-07 17:18:33 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,26 @@
+require 'pry'
+class Artist
+  attr_accessor :name
+  @@all = []
+   def initialize(name)
+     @name = name
+     @@all << self
+   end
+
+   def self.song_count
+    Song.count
+   end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+   def add_song(song)
+    song.artist = self
+   end
+
+   def add_song_by_name(song_name)
+    add_song(Song.new(song_name))
+   end
+
+end
