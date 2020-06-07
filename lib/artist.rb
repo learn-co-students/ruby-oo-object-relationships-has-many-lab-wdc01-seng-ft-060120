0bba387760386babc1199fdfadc0dc118ef1f8ba
DannyCoop <cooperdaniel5141@gmail.com>
2020-06-07 16:10:55 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,35 @@
+class Artist
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @songs = []
+        
+    end
+
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def add_song_by_name(song)
+        song = Song.new(song)
+        song.artist = self
+    end
+
+    def self.song_count
+        Song.all.count
+    end
+
+    
+
+end
+
+
---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @posts = []
+    end
+
+    def posts
+        Post.all.select do |post|
+            post.author == self
+        end
+    end
+
+    def add_post(post)
+        @posts << post
+        post.author = self
+    end
+
+    def add_post_by_title(post)
+        post = Post.new(post)
+        post.author = self
+    end
+
+    def self.post_count
+        Post.all.count
+    end
+
+
+end
